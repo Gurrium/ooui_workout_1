894b8aeb2317b3cb52b1d75e6d0ba2965134a406
gurrium <gurrium@hatena.com>
2021-03-29 22:49:15 +0900

    init

---
+++ ooui_workout_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ooui_workout_1
+//
+//  Created by Gurrium on 2021/03/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
