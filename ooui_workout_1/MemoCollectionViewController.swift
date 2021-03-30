b2d48338f06a893a77d090d0f6526d95acfdd190
gurrium <gurrium@hatena.com>
2021-03-30 22:07:32 +0900

    NavigationControllerが表示されるようにした

---
+++ ooui_workout_1/MemoCollectionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  memoCollectionViewController.swift
+//  ooui_workout_1
+//
+//  Created by Gurrium on 2021/03/29.
+//
+
+import UIKit
+
+class MemoCollectionViewController: UICollectionViewController {
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
